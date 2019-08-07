877ecd73a4445c8ce1cd2ae2392349bb4d1b5454
Gergely Imreh <gergely@balena.io>
2019-08-07 09:57:37 +0100

    Add service name to filename
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- camera-new/start.sh
+++ camera-new/start.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-if [[ "${BALENA_SERVICE_NAME}" == "camera-${PICTURE}" ]]; then
+SHORTNAME="${BALENA_SERVICE_NAME#camera-}"
+if [[ "${SHORTNAME}" == "${PICTURE}" ]]; then
   TAKE_PICTURE=yes
 else
-  echo "Set PICTURE=${BALENA_SERVICE_NAME#camera-}" to take picture in this service.
+  echo "Set PICTURE=${SHORTNAME}" to take picture in this service.
 fi
 
 while : ; do
   if [ "${TAKE_PICTURE}" == "yes" ]; then
-    FILENAME="test_$(date +"%Y%m%d_%H%M%S").jpg"
+    FILENAME="test_${SHORTNAME}_$(date +"%Y%m%d_%H%M%S").jpg"
     echo "snapping a picture via raspistill -o /data/${FILENAME}"
     raspistill -o "/data/${FILENAME}"
   fi
